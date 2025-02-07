8c48154f0af04eb497d2e0214eed68c0fd20bc14
mabindajoash <mabindajoash@gmail.com>
2025-02-07 04:26:11 -0800

    task 2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,12 +4,16 @@
 
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello():
     return "Hello HBNB!"
-@app.route("/hbnb", strict_slashes=false)
+
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
 
