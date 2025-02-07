9ce78cc1cb3ad0f114dd32f060b0a5d863544606
mabindajoash <mabindajoash@gmail.com>
2025-02-07 04:30:19 -0800

    task 3 correction

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,8 +18,9 @@ def hbnb():
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def hbnb_c():
+def hbnb_c(text):
     return f"C {text.replace('_', ' ')}"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
