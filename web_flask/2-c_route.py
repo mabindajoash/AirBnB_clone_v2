2b3e78faaf6576741efd3aefc0f22d59581ca830
mabindajoash <mabindajoash@gmail.com>
2025-02-07 04:27:54 -0800

    task 3

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,12 +6,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     return "Hello HBNB!"
-@app.route("/hbnb", strict_slashes=false)
+
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
+
+
 @app.route("/c/<text>", strict_slashes=False)
 def hbnb_c():
     return f"C {text.replace('_', ' ')}"
